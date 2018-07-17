bffbddb443d49c7e27516e83dc5bd5f59377e2bb
TinaGVI <mailto:eleonor111@list.ru>
2018-07-17 21:22:51 +0300

    1.3. JavaDoc. Code convention дополнение

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,10 +1,6 @@
-/**
-* @author TinaGVI (mailto:eleonor111@list.ru)
-* @since 17.07.2018
-*/
 package ru.job4j.calculate;
 
-public class Calculate {
+public class Calculate {	
 	public static void main (String[] args) {
 		System.out.println ("Hello World");
 	}
