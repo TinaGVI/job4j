96b4e612803cf3e5f380b8cf4ea3c0dfaa35de24
TinaGVI <mailto:eleonor111@list.ru>
2018-07-17 21:47:47 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,24 @@
 package ru.job4j.calculate;
-
-public class Calculate {	
+/**
+* Calculator.
+*
+* @author TinaGVI (mailto:eleonor111@list.ru)
+* @since 17.07.18
+*/
+public class Calculate {
+/**
+* main.
+* @param args - args.
+*/	
 	public static void main (String[] args) {
 		System.out.println ("Hello World");
 	}
+	/**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+*
+* @author TinaGVI (mailto:eleonor111@list.ru)
+* @since 17.07.2018
+*/
+package ru.job4j.calculate;
