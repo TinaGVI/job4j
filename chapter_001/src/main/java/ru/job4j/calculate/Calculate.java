f264ec6f60010b0e86a6390cd9211c9a6237b388
TinaGVI <mailto:eleonor111@list.ru>
2018-07-17 21:17:25 +0300

    1.2.1. Описание Maven pom.xml

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,11 @@
+/**
+* @author TinaGVI (mailto:eleonor111@list.ru)
+* @since 17.07.2018
+*/
+package ru.job4j.calculate;
+
+public class Calculate {
+	public static void main (String[] args) {
+		System.out.println ("Hello World");
+	}
+}
