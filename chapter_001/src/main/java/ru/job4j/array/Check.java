af2c4683599d7b3f82a09fb31230e59ec3254060
TinaGVI <mailto:eleonor111@list.ru>
2018-07-22 15:10:54 +0300

    Массив заполнен True или False (Коррекция, добавлен break)

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -16,6 +16,7 @@ public class Check {
         for (int i = 1; i < data.length; i++) {
             if (data[i] != data[i - 1]) {
                 result = false;
+                break;
             }
         }
         return result;
