259c6b22aad93ff32dff26f9bfc43a77cda68dd3
TinaGVI <mailto:eleonor111@list.ru>
2018-10-10 20:20:18 +0300

    Конвертация двумерного массива в ArrayList

---
+++ chapter_003/src/main/java/ru/job4j/search/list/ConvertMatrix2List.java
@@ -0,0 +1,22 @@
+package ru.job4j.search.list;
+
+/**
+ * Конвертация двумерного массива в ArrayList.
+ *
+ * @author TinaGVI
+ * @since 10.10.2018
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertMatrix2List {
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int[] row : array) {
+            for (int cell : row) {
+                list.add(cell);
+            }
+        }
+        return list;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/search/list/ConvertMatrix2ListTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.search.list;
+
+/**
+ * Test конвертации двумерного массива в ArrayList.
+ *
+ * @author TinaGVI
+ * @since 10.10.2018
+ */
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConvertMatrix2ListTest {
+    @Test
+    public void when2on2ArrayThenList4() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {7, 3},
+                {2, 9}
+        };
+        List<Integer> expect = Arrays.asList(
+                7, 3, 2, 9
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+}
