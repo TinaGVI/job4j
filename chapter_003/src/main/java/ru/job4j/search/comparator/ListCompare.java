eda58d76f6dd1c81544dc85bc03b482afab0e24c
TinaGVI <mailto:eleonor111@list.ru>
2018-10-24 20:57:23 +0300

    Компаратор для строк.

---
+++ chapter_003/src/main/java/ru/job4j/search/comparator/ListCompare.java
@@ -0,0 +1,40 @@
+package ru.job4j.search.comparator;
+
+import java.util.Comparator;
+
+/**
+ * Компаратор для строк (точнее двух массивов символов).
+ *
+ * @author TinaGVI
+ * @since 24.10.18
+ */
+public class ListCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int size = left.length();
+        if (left.length() < right.length()) {
+            size = right.length();
+        }
+        char[] first = toCharArray(size, left);
+        char[] second = toCharArray(size, right);
+        int result = 0;
+        for (int index = 0; index < size; index++) {
+            if (Character.compare(first[index], second[index]) < 0) {
+                result = -1;
+                break;
+            } else if (Character.compare(first[index], second[index]) > 0) {
+                result = 1;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private char[] toCharArray(int size, String string) {
+        char[] values = new char[size];
+        for (int index = 0; index < string.length(); index++) {
+            values[index] = string.charAt(index);
+        }
+        return values;
+    }
+}
