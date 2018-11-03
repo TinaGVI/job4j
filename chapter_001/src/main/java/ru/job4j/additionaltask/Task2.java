e022bd70f84646b5392715a65cd6b091c262fe81
TinaGVI <mailto:eleonor111@list.ru>
2018-11-03 20:10:12 +0300

    Тест Travis

--- chapter_001/src/main/java/ru/job4j/additionaltask/Task2.java
+++ chapter_001/src/main/java/ru/job4j/additionaltask/Task2.java
@@ -1,13 +1,13 @@
 package ru.job4j.additionaltask;
 
 public class Task2 {
-   /** int transection (int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        if ((ax1 && ay1 < ax2 && ay2) && (bx1 && by1 < bx2 && by2)){
-            int v1 = (bx2 - bx1) * (ay1 - by1) - (by2 - by1) * (ax1 - bx1);
-            int v2 = (bx2 - bx1) * (ay2 - by1) - (by2 - by1) * (ax2 - bx1);
-            int v3 = (ax2 - ax1) * (by1 - ay1) - (ay2 - ay1) * (bx1 - ax1);
-            int v4 = (ax2 - ax1) * (by2 - ay1) - (ay2 - ay1) * (bx2 - ax1);
-            return ((v1 * v2 <= 0) && (v3 * v4 <= 0));
-        }
-    }**/
+    /** int transection (int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+     if ((ax1 && ay1 < ax2 && ay2) && (bx1 && by1 < bx2 && by2)){
+     int v1 = (bx2 - bx1) * (ay1 - by1) - (by2 - by1) * (ax1 - bx1);
+     int v2 = (bx2 - bx1) * (ay2 - by1) - (by2 - by1) * (ax2 - bx1);
+     int v3 = (ax2 - ax1) * (by1 - ay1) - (ay2 - ay1) * (bx1 - ax1);
+     int v4 = (ax2 - ax1) * (by2 - ay1) - (ay2 - ay1) * (bx2 - ax1);
+     return ((v1 * v2 <= 0) && (v3 * v4 <= 0));
+     }
+     }**/
 }
